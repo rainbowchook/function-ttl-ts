a16b0d9d6988d52cd04ca59368deb4c1ea35d9b8
rainbowdev <rainbowskyworks@gmail.com>
2023-11-09 12:29:05 +1100

    Changed stack name

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { FunctionTTLGoStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new FunctionTTLGoStack(app, 'FunctionTTLGoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
